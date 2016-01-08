b2a653eb972a49634e6c00b02601f191febdac07
Joseph Leon <joseph.leon.9@gmail.com>
2016-01-08 10:18:46 -0500

    Grab the gold price for today and render it on the home page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+	require 'open-uri'
+
   def index
+  	url = 'https://www.quandl.com/api/v3/datasets/BUNDESBANK/BBK01_WT5511.json'
+  	@data = data = JSON.parse(open(url).read)
+
+  	@gold_price = @data["dataset"]["data"][0][1]
   end
 end
