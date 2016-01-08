0056649b55c256a0b13ff760500b170e821ea706
Joseph Leon <joseph.leon.9@gmail.com>
2016-01-08 13:50:46 -0500

    Add silver prices, clean up code.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,16 @@ class WelcomeController < ApplicationController
 	require 'open-uri'
 
   def index
+  	# Get and parse gold price data
   	url = 'https://www.quandl.com/api/v3/datasets/BUNDESBANK/BBK01_WT5511.json'
-  	@data = data = JSON.parse(open(url).read)
+  	@golddata = data = JSON.parse(open(url).read)
 
-  	@gold_price = @data["dataset"]["data"][0][1]
+  	# Get and parse silver price data
+  	url = 'https://www.quandl.com/api/v1/datasets/LBMA/SILVER.json'
+  	@silverdata = data = JSON.parse(open(url).read)
+
+  	# Set this days price to a variable
+  	@gold_price = @golddata["dataset"]["data"][0][1]
+  	@silver_price = @silverdata["data"][0][1]
   end
 end
