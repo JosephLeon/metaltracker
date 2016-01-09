408e4de19de008e554330da4d30236b5c93ab7a0
Joseph Leon <joseph.leon.9@gmail.com>
2016-01-09 18:08:37 -0500

    Add better errors, comment some api calls on home page until its back up.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,16 +2,18 @@ class WelcomeController < ApplicationController
 	require 'open-uri'
 
   def index
+    # Commented temporarily until quandl is back.
+    
   	# Get and parse gold price data
-  	url = 'https://www.quandl.com/api/v3/datasets/BUNDESBANK/BBK01_WT5511.json'
-  	@golddata = data = JSON.parse(open(url).read)
+  	# url = 'https://www.quandl.com/api/v3/datasets/BUNDESBANK/BBK01_WT5511.json'
+  	# @golddata = data = JSON.parse(open(url).read)
 
-  	# Get and parse silver price data
-  	url = 'https://www.quandl.com/api/v1/datasets/LBMA/SILVER.json'
-  	@silverdata = data = JSON.parse(open(url).read)
+  	# # Get and parse silver price data
+  	# url = 'https://www.quandl.com/api/v1/datasets/LBMA/SILVER.json'
+  	# @silverdata = data = JSON.parse(open(url).read)
 
-  	# Set this days price to a variable
-  	@gold_price = @golddata["dataset"]["data"][0][1]
-  	@silver_price = @silverdata["data"][0][1]
+  	# # Set this days price to a variable
+  	# @gold_price = @golddata["dataset"]["data"][0][1]
+  	# @silver_price = @silverdata["data"][0][1]
   end
 end
