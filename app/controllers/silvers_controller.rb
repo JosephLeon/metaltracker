a6eb2ca1e54e96199a2e520ff9d01ba03f7b7cc3
Joseph Leon <joseph.leon.9@gmail.com>
2016-01-08 21:02:06 -0500

    Add routes for silvers, add methods for creating and deleting silvers, build form for creating new silvers.

--- app/controllers/silvers_controller.rb
+++ app/controllers/silvers_controller.rb
@@ -1,2 +1,31 @@
 class SilversController < ApplicationController
+	def new
+		@silver = Silver.new
+	end
+
+	def create
+		@silver = Silver.new(silver_params)
+
+		@silver.save
+
+		respond_to do |format|
+			format.html { redirect_to "/" }
+			format.js
+		end
+	end
+
+	def destroy
+		@silver = Silver.find(params[:id])
+		@silver.destroy
+
+		respond_to do |format|
+			format.html { redirect_to "/" }
+			format.js
+		end
+	end
+
+	private
+		def silver_params
+			params.require(:silver).permit(:name, :silver_amount, :date_purchased, :notes, :price_paid)
+		end
 end
