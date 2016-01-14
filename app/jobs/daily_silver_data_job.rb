c750770b97182bdc2693425d75c8cb87dd800227
Joseph Leon <joseph.leon.9@gmail.com>
2016-01-14 12:40:01 -0500

    Create a daily silver data job which will email me daily with profit and silver prices and maybe other data.

---
+++ app/jobs/daily_silver_data_job.rb
@@ -0,0 +1,7 @@
+class DailySilverDataJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/daily_silver_data_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DailySilverDataJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
